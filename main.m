4ed3c29ddb0d830237bbb57be2e8c89ef0151b1f
Reza Jelveh <toyb0mb@gmx.net>
2008-11-16 15:10:37 +0100

    added resources

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RAR expander
+//
+//  Created by Reza Jelveh on 11/16/08.
+//  Copyright Acculogic GmbH 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
